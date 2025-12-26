b77cf216083b901fe43bb04684c3768ea22f5530
Bakso14 <70924870+Bakso14@users.noreply.github.com>
2025-12-26 09:19:51 +0700

    set api

--- src/config/env.php
+++ src/config/env.php
@@ -3,14 +3,14 @@
 
 define('ENV', [
     'DB_HOST' => '127.0.0.1',
-    'DB_USER' => 'root',
-    'DB_PASS' => '',
-    'DB_NAME' => 'apotikdb',
+    'DB_USER' => 'u212736227_apotek',
+    'DB_PASS' => 'Ana_Cantik14',
+    'DB_NAME' => 'u212736227_apotek',
     'DB_PORT' => 3306,
     // Application/server timezone (PHP)
     'TIMEZONE' => 'Asia/Jakarta',
     // MySQL session time zone. Use "+07:00" for WIB
     'DB_TIMEZONE' => '+07:00',
-    'REQUIRE_API_KEY' => false,
-    'API_KEY' => 'dev-key',
+    'REQUIRE_API_KEY' => true,
+    'API_KEY' => 'base64:1MoMxXbkcXefzFjyUbASvgKkczbMYlT83OL+wXJGaF8=',
 ]);
